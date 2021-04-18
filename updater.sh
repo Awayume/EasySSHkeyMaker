3fa02e5736e820c1dd10186bc14ac3fc4ccf80d7
粟夢 <79361022+Awayume@users.noreply.github.com>
2021-04-18 13:26:08 +0900

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,1 +1,18 @@
+#!/bin/sh
 
+# 新バージョンの案内
+echo Ver2.1
+echo 
+# アップデート開始文
+echo アップデートをスタートします。Enterを押してください。
+read Wait
+# 旧バージョンの削除
+cd $home
+rm maker.sh
+# 新バージョンのダウンロード
+wget https://github.com/Awayume/EasySSHkeyMaker/releases/download/v2.1/maker.sh
+chmod 755 maker.sh
+echo アップデートが完了しました。ツールを起動します。
+echo Enterを押してください。
+read Wait
+./maker.sh
